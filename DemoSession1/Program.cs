8f0581f1a88b8b89855acf533d66c6f835f72a45
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-06-23 22:10:48 +0300

    Add project files.

---
+++ DemoSession1/Program.cs
@@ -0,0 +1,95 @@
+﻿
+using System.Xml.Linq;
+
+// Video : FirstProject With VisualStudio & See IL Code 
+
+/* namespace HelloWorld // Main namespace 
+{
+    // Class , Enum , Struct , Interface 
+    internal class Program
+    {
+ static void (string[] args)			// Entry Point
+{
+console.writeline("Hello , World ! " ) ; 
+// Console : Class 
+// Writeline : Function
+}
+}
+}
+*/
+
+
+
+// Video Comments & Regions
+/*
+namespace SessionTwo
+{
+    internal class Program
+    {
+        /// <summary> 
+        /// this function can sum two int numbers
+        /// </summary> 
+        /// <param name="num1">this is the first number </param> 
+        /// <param name="num1">this is the Second number </param> 
+        /// <returns > the result will be the sum of num1 + num 2  </returns > 
+
+        static int (int num1, int num2)			
+{
+return  num1 + num2 ;
+}
+
+
+*/ 
+
+/*
+    /// <summary> 
+    /// 
+    /// </summary> 
+    /// <param name="x"></param> 
+    Static void Main(int x)
+    {
+# Region Comments and regions 
+        // this is my first comment [ single Line Comment ] 
+        // int x = 5 ; 
+        // int y = 5 ; 
+
+        /*
+        * this is my first Multiple line comment
+        * int x = 5 ; 
+        * int y = 10 ;  
+        
+
+#endregion
+*/
+
+/*
+
+# Region Question 01
+        // int x = 10 ; 
+        // int y = 20 ;
+        // console.writeline(x+y) ; 
+        // console.writeline(" Hello " ) ; 
+
+
+    #endregion
+
+
+        // int x = 5 ; 		// 4 bytes
+
+# Region Variables declaration and naming conviention
+
+        // int studentAge = 24 ; 	// 4 bytes in stack [ value = 20 ] 
+        // console.writeline(studentAge) ; 
+        // DataType Name ] camelCase] Value
+        // int 9number; 	//invalid
+        // int @class = 200 ; 
+        //  int @struct = 200 ; 
+        // console.writeline(@class) ; 
+
+
+#endregion
+
+    }
+}
+
+*/ 
